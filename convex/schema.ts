b326d645406f0e84a916417d11f1c479dcbfe227
ghxstling <dcho282@aucklanduni.ac.nz>
2024-12-10 12:33:44 +1300

    feat: create schema for convex

---
+++ convex/schema.ts
@@ -0,0 +1,26 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  classroomCodes: defineTable({
+    code: v.string(),
+  }),
+
+  users: defineTable({
+    firstName: v.string(),
+    lastName: v.string(),
+    email: v.id("users"),
+    password: v.string(),
+    type: v.string(),
+    code: v.id("classroomCodes"),
+  }).index("by_email", ["email"]),
+
+  tasks: defineTable({
+    title: v.string(),
+    description: v.optional(v.string()),
+    dueDate: v.string(),
+    completed: v.boolean(),
+    assignedTo: v.id("users"),
+    assignedBy: v.id("users"),
+  }).index("by_dueDate", ["dueDate"]),
+});
