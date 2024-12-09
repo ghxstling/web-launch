6ce012c702edb5d3ab91dd31274e228dc6219517
ghxstling <dcho282@aucklanduni.ac.nz>
2024-12-09 16:17:07 +1300

    feat: integrate Clerk and Convex with Next.js

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://absolute-bobcat-28.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,46 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Copyright, CopyrightIcon } from "lucide-react";
-import Link from "next/link";
+import {
+  SignInButton,
+  ClerkLoaded,
+  SignUpButton,
+  useUser,
+} from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { isSignedIn } = useUser();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  });
+
   return (
     <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-16 items-center">
-        <div className="flex flex-col gap-3 items-center">
-          <h1 className="text-5xl font-semibold">Welcome to LearnQuest!</h1>
-          <p className="text-xl">Education at Your Fingertips</p>
-        </div>
-        <div className="flex flex-row gap-4 items-center ">
-          <Button className="rounded-full border border-solid border-transparent text-base min-h-12 min-w-32">
-            <Link href="/register">Sign up</Link>
-          </Button>
-          <Button className="rounded-full border border-solid border-transparent text-base min-h-12 min-w-32">
-            <Link href="/login">Log in</Link>
-          </Button>
-        </div>
+      <main className="flex flex-col gap-12 items-center">
+        <ClerkLoaded>
+          <div className="flex flex-col gap-3 items-center">
+            <h1 className="text-5xl font-semibold">Welcome to LearnQuest!</h1>
+            <p className="text-xl">Education at Your Fingertips</p>
+          </div>
+          <div className="flex flex-row gap-8 items-center ">
+            <SignInButton>
+              <Button className="rounded-full border border-solid border-transparent text-base min-h-12 min-w-32">
+                Log In
+              </Button>
+            </SignInButton>
+            <SignUpButton>
+              <Button className="rounded-full border border-solid border-transparent text-base min-h-12 min-w-32">
+                Sign Up
+              </Button>
+            </SignUpButton>
+          </div>
+        </ClerkLoaded>
       </main>
     </div>
   );
