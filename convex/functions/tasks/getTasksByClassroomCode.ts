5927b5ee4fe444643efa420a57db0ab51ff03a13
ghxstling <dcho282@aucklanduni.ac.nz>
2024-12-20 22:00:54 +1300

    feat: rename getTasksAssignedToUser function to getTasksByClassroomCode and update query

--- convex/functions/tasks/getTasksByClassroomCode.ts
+++ convex/functions/tasks/getTasksByClassroomCode.ts
@@ -3,12 +3,12 @@ import { v } from "convex/values";
 
 export default query({
   args: {
-    assignedTo: v.id("users"),
+    code: v.string(),
   },
   handler: async (ctx, args) => {
     return await ctx.db
       .query("tasks")
-      .filter((q) => q.eq(q.field("assignedTo"), args.assignedTo))
+      .filter((q) => q.eq(q.field("code"), args.code))
       .collect();
   },
 });
