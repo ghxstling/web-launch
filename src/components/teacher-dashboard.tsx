7df2a952d625b077cfb64336e0b4ee7dadf0c3ff
ghxstling <dcho282@aucklanduni.ac.nz>
2024-12-22 00:36:11 +1300

    feat: enable each item in list to be expandable, showing more details

--- src/components/teacher-dashboard.tsx
+++ src/components/teacher-dashboard.tsx
@@ -3,6 +3,12 @@
 import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { useConvex } from "convex/react";
 import { useEffect, useState } from "react";
 import { useTasksService } from "../../convex/services/tasksService";
@@ -10,6 +16,7 @@ import { useUserService } from "../../convex/services/userService";
 import { CardContent, CardFooter } from "./ui/card";
 import { Button } from "./ui/button";
 import { ChevronRight } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
 
 interface TeacherDashboardProps {
   user: any;
@@ -17,11 +24,12 @@ interface TeacherDashboardProps {
 
 const css = {
   noData: "flex justify-center text-sm text-gray-400 mt-48",
-  listEntry: "flex justify-between items-center",
-  listEntryText: "text-sm truncate max-w-[calc(100%-3rem)]",
+  listEntryTitle: "text-sm font-medium truncate max-w-sm",
+  listEntryDesc: "text-wrap w-[30rem]",
 };
 
 export function TeacherDashboard({ user }: TeacherDashboardProps) {
+  const { isLoaded } = useUser();
   const convex = useConvex();
   const userService = useUserService(convex);
   const tasksService = useTasksService(convex);
@@ -31,88 +39,147 @@ export function TeacherDashboard({ user }: TeacherDashboardProps) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const students = (
-        await userService.getUsersByClassroomCode(user.code!)
-      ).filter((u) => u.type === "student");
-      const tasks = await tasksService.getTasksAssignedByUser(user._id);
+      try {
+        const students = (
+          await userService.getUsersByClassroomCode(user.code!)
+        ).filter((u) => u.type === "student");
+        const tasks = await tasksService.getTasksAssignedByUser(user._id);
 
-      setStudents(students);
-      setTasks(tasks);
+        setStudents(students);
+        setTasks(tasks);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    fetchData();
+    if (isLoaded) fetchData();
+    return;
   }, [user, userService, tasksService]);
 
-  let studentsList, tasksList;
-  if (students.length !== 0) {
-    studentsList = students.map((student) => (
-      <React.Fragment key={student._id}>
-        <div className={css.listEntry}>
-          <p className={css.listEntryText}>
-            {student.firstName} {student.lastName}
-          </p>
-          <Button variant={"ghost"} className="size-6 p-0 hover:bg-none">
-            <ChevronRight />
-          </Button>
-        </div>
-        <Separator className="my-2" />
-      </React.Fragment>
-    ));
-  } else {
-    studentsList = <div className={css.noData}>No students available</div>;
-  }
+  if (isLoaded) {
+    let studentsList, tasksList;
+    if (students.length !== 0) {
+      studentsList = students.map((student) => (
+        <AccordionItem value={student._id} key={student._id}>
+          <AccordionTrigger
+            className={
+              (students.findIndex((s) => s._id === student._id) === 0
+                ? "mb-2"
+                : "my-2") + " py-0"
+            }
+          >
+            <p className={css.listEntryTitle}>
+              {student.firstName} {student.lastName}
+            </p>
+          </AccordionTrigger>
+          <AccordionContent>
+            <Separator className="mb-4" />
+            <ul className={css.listEntryDesc + " w-60"}>
+              <li>
+                <b>Email:</b> {student.email}
+              </li>
+              <li>
+                <b>Tasks Completed:</b> {student.completedTasks.length} out of{" "}
+                {tasks.length} tasks
+              </li>
+              <li>
+                <b>Last logged in:</b> random date
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      ));
+    } else {
+      studentsList = <div className={css.noData}>No students available</div>;
+    }
 
-  if (tasks.length !== 0) {
-    tasksList = tasks.map((task) => (
-      <React.Fragment key={task._id}>
-        <div className={css.listEntry}>
-          <p className={css.listEntryText}>{task.title}</p>
-          <Button variant={"ghost"} className="size-6 p-0 hover:bg-none">
-            <ChevronRight />
-          </Button>
-        </div>
-        <Separator className="my-2" />
-      </React.Fragment>
-    ));
-  } else {
-    tasksList = <div className={css.noData}>No tasks assigned</div>;
-  }
+    if (tasks.length !== 0) {
+      tasksList = tasks.map((task) => (
+        <AccordionItem value={task._id} key={task._id}>
+          <AccordionTrigger
+            className={
+              (tasks.findIndex((s) => s._id === task._id) === 0
+                ? "mb-2"
+                : "my-2") + " py-0"
+            }
+          >
+            <p className={css.listEntryTitle}>{task.title}</p>
+          </AccordionTrigger>
+          <AccordionContent>
+            <Separator className="mb-4" />
+            <ul className={css.listEntryDesc}>
+              <li>
+                <b>Description:</b>{" "}
+                {task.description ? task.description : "N/A"}
+              </li>
+              <li>
+                <b>Due Date:</b>{" "}
+                {new Date(task.dueDate).toLocaleDateString("en-NZ", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </li>
+              <li>
+                <b>Tasks Completed:</b>{" "}
+                {
+                  tasks.filter(
+                    (t) => t.completed === true && t.code === task.code,
+                  ).length
+                }{" "}
+                out of {tasks.length} tasks
+              </li>
+              <li>
+                <b>Last logged in:</b> random date
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      ));
+    } else {
+      tasksList = <div className={css.noData}>No tasks assigned</div>;
+    }
 
-  const headings = ["Students", "Assigned Tasks"];
-  const data = [studentsList, tasksList];
-  const lists = Array.from({ length: 2 }).map((_, i) => (
-    <div
-      key={i}
-      className={
-        `flex flex-col ` +
-        (i === 0 ? "w-[calc(33%-0.5rem)]" : "w-[calc(67%-0.5rem)]")
-      }
-    >
-      <h1 className="text-center text-xl font-medium underline underline-offset-1 mt-2 mb-4">
-        {headings.at(i)}
-      </h1>
-      <ScrollArea
-        type="hover"
-        className="w-full h-[40rem] rounded-md border p-4"
+    const headings = ["Students", "Assigned Tasks"];
+    const data = [studentsList, tasksList];
+    const lists = Array.from({ length: 2 }).map((_, i) => (
+      <div
+        key={i}
+        className={
+          `flex flex-col ` +
+          (i === 0
+            ? "w-[calc(40%-0.5rem)] max-w-[40%]"
+            : "w-[calc(60%-0.5rem)] max-w-[60%]")
+        }
       >
-        {data.at(i)}
-      </ScrollArea>
-    </div>
-  ));
+        <h1 className="text-center text-xl font-medium underline underline-offset-1 mt-2 mb-4">
+          {headings.at(i)}
+        </h1>
+        <ScrollArea
+          type="hover"
+          className="w-full h-[40rem] rounded-md border p-4"
+        >
+          <Accordion type="single" collapsible className="w-full">
+            {data.at(i)}
+          </Accordion>
+        </ScrollArea>
+      </div>
+    ));
 
-  return (
-    <>
-      <CardContent className="py-0 h-[80%]">
-        <div className="flex flex-row justify-between">
-          {lists.map((list) => list)}
-        </div>
-      </CardContent>
-      <CardFooter className="grid grid-flow-col pt-6 h-[10%]">
-        <div className="flex gap-2 justify-end">
-          <Button>Create New Task</Button>
-          <Button>Invite Student</Button>
-        </div>
-      </CardFooter>
-    </>
-  );
+    return (
+      <>
+        <CardContent className="py-0 h-[80%]">
+          <div className="flex flex-row justify-between w-full">
+            {lists.map((list) => list)}
+          </div>
+        </CardContent>
+        <CardFooter className="grid grid-flow-col pt-6 h-[10%]">
+          <div className="flex gap-2 justify-end">
+            <Button>Create New Task</Button>
+            <Button>Invite Student</Button>
+          </div>
+        </CardFooter>
+      </>
+    );
+  }
 }
