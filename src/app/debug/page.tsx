98ac2c8a99f9288701ae0444cdab35969878cd3a
ghxstling <dcho282@aucklanduni.ac.nz>
2024-12-22 00:32:38 +1300

    feat: add generateData and resetDatabase to debug page

--- src/app/debug/page.tsx
+++ src/app/debug/page.tsx
@@ -1,14 +1,11 @@
-// Used for debugging purposes only
-// This is an easy way to test auth and roles for backend endpoints
 "use client";
 
 import { useEffect, useState } from "react";
 import { SignOutButton, SignInButton, useUser } from "@clerk/nextjs";
 import { useUserService } from "../../../convex/services/userService";
-import { Id } from "../../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { useGenerateData } from "./seed";
-import { useConvex, useMutation } from "convex/react";
+import { useConvex } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
 export default function DebugPage() {
@@ -16,7 +13,7 @@ export default function DebugPage() {
   const { isLoaded, isSignedIn, user } = useUser();
   const userService = useUserService(convex);
 
-  const [apiResponse, setApiResponse] = useState(null);
+  const [apiResponse, setApiResponse] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
   const [apiUser, setApiUser] = useState<any>(null);
 
@@ -25,7 +22,6 @@ export default function DebugPage() {
       if (user) {
         const session = user!.primaryEmailAddress!.emailAddress;
         let apiUser = await userService.getUserByEmail(session);
-        console.log(apiUser);
         setApiUser(apiUser);
       }
     };
@@ -35,24 +31,42 @@ export default function DebugPage() {
     }
   }, [isLoaded, isSignedIn, userService.getUserByEmail]);
 
-  const resetDatabase = async () => {
-    await convex.mutation(api.functions.resetDatabase.default);
+  const generateData = async () => {
+    setError(null);
+    setApiResponse({ message: "Generating data..." });
+    useGenerateData(123, convex)
+      .then(() => {
+        setApiResponse({ message: "Seeding completed" });
+      })
+      .catch((err) => {
+        setError("Error seeding data: " + err);
+        console.log(err);
+      });
   };
 
-  const makeApiCall = async () => {
-    try {
-      const res = await fetch("/api/courses", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      setApiResponse(await res.json());
-      setError(null);
-    } catch (err) {
-      setError("Error fetching data from the API");
-      setApiResponse(null);
-    }
+  const resetDatabase = async () => {
+    await convex.mutation(api.functions.resetDatabase.default);
+    await convex.mutation(api.functions.users.addUser.default, {
+      firstName: "TEST",
+      lastName: "TEACHER",
+      email: "dylan.choy21@gmail.com",
+      password: "12341234",
+      confirm: "12341234",
+      type: "teacher",
+      code: "",
+      completedTasks: [],
+    });
+    await convex.mutation(api.functions.users.addUser.default, {
+      firstName: "TEST",
+      lastName: "STUDENT",
+      email: "dcho282@aucklanduni.ac.nz",
+      password: "12341234",
+      confirm: "12341234",
+      type: "student",
+      code: "",
+      completedTasks: [],
+    });
+    setApiResponse({ message: "Database has been reset" });
   };
 
   return (
@@ -78,10 +92,7 @@ export default function DebugPage() {
           </SignInButton>
         </>
       )}
-      <Button onClick={makeApiCall}>Make API Call</Button>
-      <Button onClick={async () => useGenerateData(123, convex)}>
-        Generate Data
-      </Button>
+      <Button onClick={generateData}>Generate Data</Button>
       <Button onClick={resetDatabase}>Reset Database</Button>
       {error && <p style={{ color: "red" }}>{error}</p>}
       {apiResponse && (
